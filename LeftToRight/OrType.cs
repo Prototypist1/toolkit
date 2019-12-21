027336f89e3e3daf584b2661939be38e3d535418
Colin Wielga <colinwielga@gmail.com>
2019-12-20 20:57:54 -0500

    added Equals

--- LeftToRight/OrType.cs
+++ LeftToRight/OrType.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Prototypist.Toolbox
 {
-    public class OrType<T1, T2>
+    public class OrType<T1, T2> : IEquatable<OrType<T1, T2>>
     {
         private readonly T1 t1;
         private readonly bool isT1;
@@ -47,9 +48,33 @@ namespace Prototypist.Toolbox
             }
             throw new ApplicationException($"expect {typeof(T2).FullName}");
         }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as OrType<T1, T2>);
+        }
+
+        public bool Equals(OrType<T1, T2> other)
+        {
+            return other != null &&
+                   EqualityComparer<T1>.Default.Equals(t1, other.t1) &&
+                   isT1 == other.isT1 &&
+                   EqualityComparer<T2>.Default.Equals(t2, other.t2) &&
+                   isT2 == other.isT2;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -447460166;
+            hashCode = hashCode * -1521134295 + EqualityComparer<T1>.Default.GetHashCode(t1);
+            hashCode = hashCode * -1521134295 + isT1.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<T2>.Default.GetHashCode(t2);
+            hashCode = hashCode * -1521134295 + isT2.GetHashCode();
+            return hashCode;
+        }
     }
 
-    public class OrType<T1, T2, T3>
+    public class OrType<T1, T2, T3> : IEquatable<OrType<T1, T2, T3>>
     {
         private readonly T1 t1;
         private readonly bool isT1;
@@ -119,9 +144,37 @@ namespace Prototypist.Toolbox
             }
             throw new ApplicationException($"expect {typeof(T3).FullName}");
         }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as OrType<T1, T2, T3>);
+        }
+
+        public bool Equals(OrType<T1, T2, T3> other)
+        {
+            return other != null &&
+                   EqualityComparer<T1>.Default.Equals(t1, other.t1) &&
+                   isT1 == other.isT1 &&
+                   EqualityComparer<T2>.Default.Equals(t2, other.t2) &&
+                   isT2 == other.isT2 &&
+                   EqualityComparer<T3>.Default.Equals(t3, other.t3) &&
+                   isT3 == other.isT3;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 2049221290;
+            hashCode = hashCode * -1521134295 + EqualityComparer<T1>.Default.GetHashCode(t1);
+            hashCode = hashCode * -1521134295 + isT1.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<T2>.Default.GetHashCode(t2);
+            hashCode = hashCode * -1521134295 + isT2.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<T3>.Default.GetHashCode(t3);
+            hashCode = hashCode * -1521134295 + isT3.GetHashCode();
+            return hashCode;
+        }
     }
 
-    public class OrType<T1, T2, T3, T4>
+    public class OrType<T1, T2, T3, T4> : IEquatable<OrType<T1, T2, T3, T4>>
     {
         private readonly T1 t1;
         private readonly bool isT1;
@@ -213,9 +266,41 @@ namespace Prototypist.Toolbox
             }
             throw new ApplicationException($"expect {typeof(T4).FullName}");
         }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as OrType<T1, T2, T3, T4>);
+        }
+
+        public bool Equals(OrType<T1, T2, T3, T4> other)
+        {
+            return other != null &&
+                   EqualityComparer<T1>.Default.Equals(t1, other.t1) &&
+                   isT1 == other.isT1 &&
+                   EqualityComparer<T2>.Default.Equals(t2, other.t2) &&
+                   isT2 == other.isT2 &&
+                   EqualityComparer<T3>.Default.Equals(t3, other.t3) &&
+                   isT3 == other.isT3 &&
+                   EqualityComparer<T4>.Default.Equals(t4, other.t4) &&
+                   isT4 == other.isT4;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1611974508;
+            hashCode = hashCode * -1521134295 + EqualityComparer<T1>.Default.GetHashCode(t1);
+            hashCode = hashCode * -1521134295 + isT1.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<T2>.Default.GetHashCode(t2);
+            hashCode = hashCode * -1521134295 + isT2.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<T3>.Default.GetHashCode(t3);
+            hashCode = hashCode * -1521134295 + isT3.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<T4>.Default.GetHashCode(t4);
+            hashCode = hashCode * -1521134295 + isT4.GetHashCode();
+            return hashCode;
+        }
     }
     
-    public class OrType<T1, T2, T3, T4, T5>
+    public class OrType<T1, T2, T3, T4, T5> : IEquatable<OrType<T1, T2, T3, T4, T5>>
     {
         private readonly T1 t1;
         private readonly bool isT1;
@@ -329,6 +414,42 @@ namespace Prototypist.Toolbox
                 return t5;
             }
             throw new ApplicationException($"expect {typeof(T5).FullName}");
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as OrType<T1, T2, T3, T4, T5>);
+        }
+
+        public bool Equals(OrType<T1, T2, T3, T4, T5> other)
+        {
+            return other != null &&
+                   EqualityComparer<T1>.Default.Equals(t1, other.t1) &&
+                   isT1 == other.isT1 &&
+                   EqualityComparer<T2>.Default.Equals(t2, other.t2) &&
+                   isT2 == other.isT2 &&
+                   EqualityComparer<T3>.Default.Equals(t3, other.t3) &&
+                   isT3 == other.isT3 &&
+                   EqualityComparer<T4>.Default.Equals(t4, other.t4) &&
+                   isT4 == other.isT4 &&
+                   EqualityComparer<T5>.Default.Equals(t5, other.t5) &&
+                   isT5 == other.isT5;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -527366096;
+            hashCode = hashCode * -1521134295 + EqualityComparer<T1>.Default.GetHashCode(t1);
+            hashCode = hashCode * -1521134295 + isT1.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<T2>.Default.GetHashCode(t2);
+            hashCode = hashCode * -1521134295 + isT2.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<T3>.Default.GetHashCode(t3);
+            hashCode = hashCode * -1521134295 + isT3.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<T4>.Default.GetHashCode(t4);
+            hashCode = hashCode * -1521134295 + isT4.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<T5>.Default.GetHashCode(t5);
+            hashCode = hashCode * -1521134295 + isT5.GetHashCode();
+            return hashCode;
         }
     }
 }
