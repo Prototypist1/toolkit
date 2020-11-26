f0ad157f19e85fc1775e1202a0d084a29ea75928
Prototypist1 <ColinWielga@outlook.com>
2020-11-25 23:51:34 -0500

    added another or

--- LeftToRight/IOrType.cs
+++ LeftToRight/IOrType.cs
@@ -101,6 +101,28 @@ namespace Prototypist.Toolbox
         T SwitchReturns<T>(Func<T1, T> f1, Func<T2, T> f2, Func<T3, T> f3, Func<T4, T> f4, Func<T5, T> f5, Func<T6, T> f6, Func<T7, T> f7);
     }
 
+    public interface IOrType<out T1, out T2, out T3, out T4, out T5, out T6, out T7, out T8>
+    {
+        bool Is<T>(out T res);
+        T1 Is1OrThrow();
+        T2 Is2OrThrow();
+        T3 Is3OrThrow();
+        T4 Is4OrThrow();
+        T5 Is5OrThrow();
+        T6 Is6OrThrow();
+        T7 Is7OrThrow();
+        T8 Is8OrThrow();
+        IIsPossibly<T1> Possibly1();
+        IIsPossibly<T2> Possibly2();
+        IIsPossibly<T3> Possibly3();
+        IIsPossibly<T4> Possibly4();
+        IIsPossibly<T5> Possibly5();
+        IIsPossibly<T6> Possibly6();
+        IIsPossibly<T7> Possibly7();
+        IIsPossibly<T8> Possibly8();
+        void Switch(Action<T1> a1, Action<T2> a2, Action<T3> a3, Action<T4> a4, Action<T5> a5, Action<T6> a6, Action<T7> a7, Action<T8> a8);
+        T SwitchReturns<T>(Func<T1, T> f1, Func<T2, T> f2, Func<T3, T> f3, Func<T4, T> f4, Func<T5, T> f5, Func<T6, T> f6, Func<T7, T> f7, Func<T8, T> f8);
+    }
     public static class OrTypeExtensions
     {
 
@@ -609,6 +631,133 @@ namespace Prototypist.Toolbox
                 return true;
             }
             t7 = default;
+            return false;
+        }
+
+
+        public static T GetValueAs<T1, T2, T3, T4, T5, T6, T7, T8, T>(this IOrType<T1, T2, T3, T4, T5, T6, T7, T8> self)
+            where T1 : T
+            where T2 : T
+            where T3 : T
+            where T4 : T
+            where T5 : T
+            where T6 : T
+            where T7 : T
+            where T8 : T
+        {
+            if (self.Is<T>(out var res))
+            {
+                return res;
+            }
+            throw new Exception($"{self}'s value is not a {typeof(T).Name}");
+        }
+
+        public static T GetValueAs<T1, T2, T3, T4, T5, T6, T7, T8, T>(this IOrType<T1, T2, T3, T4, T5, T6, T7, T8> self, out T t)
+            where T1 : T
+            where T2 : T
+            where T3 : T
+            where T4 : T
+            where T5 : T
+            where T6 : T
+            where T7 : T
+            where T8 : T
+        {
+            if (self.Is<T>(out var res))
+            {
+                t = res;
+                return res;
+            }
+            throw new Exception($"{self}'s value is not a {typeof(T).Name}");
+        }
+
+        public static T IsOrThrow<T1, T2, T3, T4, T5, T6, T7, T8, T>(this IOrType<T1, T2, T3, T4, T5, T6, T7, T8> self)
+        {
+            if (self.Is<T>(out var v))
+            {
+                return v;
+            }
+            throw new Exception($"{self}'s value is not a {typeof(T).Name}");
+        }
+        public static bool Is1<T1, T2, T3, T4, T5, T6, T7, T8>(this IOrType<T1, T2, T3, T4, T5, T6, T7, T8> self, out T1 t1)
+        {
+            if (self.Possibly1() is IIsDefinately<T1> definate)
+            {
+                t1 = definate.Value;
+                return true;
+            }
+            t1 = default;
+            return false;
+        }
+        public static bool Is2<T1, T2, T3, T4, T5, T6, T7, T8>(this IOrType<T1, T2, T3, T4, T5, T6, T7, T8> self, out T2 t2)
+        {
+            if (self.Possibly2() is IIsDefinately<T2> definate)
+            {
+                t2 = definate.Value;
+                return true;
+            }
+            t2 = default;
+            return false;
+        }
+        public static bool Is3<T1, T2, T3, T4, T5, T6, T7, T8>(this IOrType<T1, T2, T3, T4, T5, T6, T7, T8> self, out T3 t3)
+        {
+            if (self.Possibly3() is IIsDefinately<T3> definate)
+            {
+                t3 = definate.Value;
+                return true;
+            }
+            t3 = default;
+            return false;
+        }
+        public static bool Is4<T1, T2, T3, T4, T5, T6, T7, T8>(this IOrType<T1, T2, T3, T4, T5, T6, T7, T8> self, out T4 t4)
+        {
+            if (self.Possibly4() is IIsDefinately<T4> definate)
+            {
+                t4 = definate.Value;
+                return true;
+            }
+            t4 = default;
+            return false;
+        }
+        public static bool Is5<T1, T2, T3, T4, T5, T6, T7, T8>(this IOrType<T1, T2, T3, T4, T5, T6, T7> self, out T5 t5)
+        {
+            if (self.Possibly5() is IIsDefinately<T5> definate)
+            {
+                t5 = definate.Value;
+                return true;
+            }
+            t5 = default;
+            return false;
+        }
+
+        public static bool Is6<T1, T2, T3, T4, T5, T6, T7, T8>(this IOrType<T1, T2, T3, T4, T5, T6, T7, T8> self, out T6 t6)
+        {
+            if (self.Possibly6() is IIsDefinately<T6> definate)
+            {
+                t6 = definate.Value;
+                return true;
+            }
+            t6 = default;
+            return false;
+        }
+
+        public static bool Is7<T1, T2, T3, T4, T5, T6, T7, T8>(this IOrType<T1, T2, T3, T4, T5, T6, T7, T8> self, out T7 t7)
+        {
+            if (self.Possibly7() is IIsDefinately<T7> definate)
+            {
+                t7 = definate.Value;
+                return true;
+            }
+            t7 = default;
+            return false;
+        }
+        public static bool Is8<T1, T2, T3, T4, T5, T6, T7, T8>(this IOrType<T1, T2, T3, T4, T5, T6, T7, T8> self, out T8 t8)
+        {
+            if (self.Possibly8() is IIsDefinately<T8> definate)
+            {
+                t8 = definate.Value;
+                return true;
+            }
+            t8 = default;
             return false;
         }
     }
--- LeftToRight/OrType.cs
+++ LeftToRight/OrType.cs
@@ -25,6 +25,7 @@ namespace Prototypist.Toolbox
     public abstract class p<T1, T2, T3, T4, T5> : p<T1, T2, T3, T4>, IIsPossibly<T5> { }
     public abstract class p<T1, T2, T3, T4, T5, T6> : p<T1, T2, T3, T4, T5>, IIsPossibly<T6> { }
     public abstract class p<T1, T2, T3, T4, T5, T6,T7> : p<T1, T2, T3, T4, T5,T6>, IIsPossibly<T7> { }
+    public abstract class p<T1, T2, T3, T4, T5, T6, T7,T8> : p<T1, T2, T3, T4, T5, T6,T7>, IIsPossibly<T8> { }
 
 
     public interface IAmRepresented {
@@ -734,6 +735,182 @@ namespace Prototypist.Toolbox
         }
     }
 
+
+#pragma warning disable CS0659 //
+    public abstract class OrType<T1, T2, T3, T4, T5, T6, T7, T8> : p<T1, T2, T3, T4, T5, T6, T7, T8>, IOrType<T1, T2, T3, T4, T5, T6, T7, T8>
+#pragma warning restore CS0659 // Type overrides Object.Equals(object o) but does not override Object.GetHashCode()
+    {
+        public bool Is<T>(out T res)
+        {
+            if (Representative() is T t)
+            {
+                res = t;
+                return true;
+            }
+            res = default;
+            return false;
+        }
+
+        public T1 Is1OrThrow()
+        {
+            if (this is IIsDefinately<T1> definate)
+            {
+                return definate.Value;
+            }
+            throw new Exception($"is not {typeof(T1)}");
+        }
+
+        public T2 Is2OrThrow()
+        {
+            if (this is IIsDefinately<T2> definate)
+            {
+                return definate.Value;
+            }
+            throw new Exception($"is not {typeof(T2)}");
+        }
+        public T3 Is3OrThrow()
+        {
+            if (this is IIsDefinately<T3> definate)
+            {
+                return definate.Value;
+            }
+            throw new Exception($"is not {typeof(T3)}");
+        }
+        public T4 Is4OrThrow()
+        {
+            if (this is IIsDefinately<T4> definate)
+            {
+                return definate.Value;
+            }
+            throw new Exception($"is not {typeof(T4)}");
+        }
+        public T5 Is5OrThrow()
+        {
+            if (this is IIsDefinately<T5> definate)
+            {
+                return definate.Value;
+            }
+            throw new Exception($"is not {typeof(T5)}");
+        }
+        public T6 Is6OrThrow()
+        {
+            if (this is IIsDefinately<T6> definate)
+            {
+                return definate.Value;
+            }
+            throw new Exception($"is not {typeof(T6)}");
+        }
+        public T7 Is7OrThrow()
+        {
+            if (this is IIsDefinately<T7> definate)
+            {
+                return definate.Value;
+            }
+            throw new Exception($"is not {typeof(T7)}");
+        }
+        public T8 Is8OrThrow()
+        {
+            if (this is IIsDefinately<T8> definate)
+            {
+                return definate.Value;
+            }
+            throw new Exception($"is not {typeof(T8)}");
+        }
+        public IIsPossibly<T1> Possibly1() => this;
+        public IIsPossibly<T2> Possibly2() => this;
+        public IIsPossibly<T3> Possibly3() => this;
+        public IIsPossibly<T4> Possibly4() => this;
+        public IIsPossibly<T5> Possibly5() => this;
+        public IIsPossibly<T6> Possibly6() => this;
+        public IIsPossibly<T7> Possibly7() => this;
+        public IIsPossibly<T8> Possibly8() => this;
+        public void Switch(Action<T1> a1, Action<T2> a2, Action<T3> a3, Action<T4> a4, Action<T5> a5, Action<T6> a6, Action<T7> a7, Action<T8> a8)
+        {
+            if (this is IIsDefinately<T1> definate1)
+            {
+                a1(definate1.Value);
+            }
+            else if (this is IIsDefinately<T2> definate2)
+            {
+                a2(definate2.Value);
+            }
+            else if (this is IIsDefinately<T3> definate3)
+            {
+                a3(definate3.Value);
+            }
+            else if (this is IIsDefinately<T4> definate4)
+            {
+                a4(definate4.Value);
+            }
+            else if (this is IIsDefinately<T5> definate5)
+            {
+                a5(definate5.Value);
+            }
+            else if (this is IIsDefinately<T6> definate6)
+            {
+                a6(definate6.Value);
+            }
+            else if (this is IIsDefinately<T7> definate7)
+            {
+                a7(definate7.Value);
+            }
+            else if (this is IIsDefinately<T8> definate8)
+            {
+                a8(definate8.Value);
+            }
+            else
+            {
+                throw new Exception("bug!");
+            }
+        }
+
+        public T SwitchReturns<T>(Func<T1, T> f1, Func<T2, T> f2, Func<T3, T> f3, Func<T4, T> f4, Func<T5, T> f5, Func<T6, T> f6, Func<T7, T> f7, Func<T8, T> f8)
+        {
+            if (this is IIsDefinately<T1> definate1)
+            {
+                return f1(definate1.Value);
+            }
+            if (this is IIsDefinately<T2> definate2)
+            {
+                return f2(definate2.Value);
+            }
+            if (this is IIsDefinately<T3> definate3)
+            {
+                return f3(definate3.Value);
+            }
+            if (this is IIsDefinately<T4> definate4)
+            {
+                return f4(definate4.Value);
+            }
+            if (this is IIsDefinately<T5> definate5)
+            {
+                return f5(definate5.Value);
+            }
+            if (this is IIsDefinately<T6> definate6)
+            {
+                return f6(definate6.Value);
+            }
+            if (this is IIsDefinately<T7> definate7)
+            {
+                return f7(definate7.Value);
+            }
+            if (this is IIsDefinately<T8> definate8)
+            {
+                return f8(definate8.Value);
+            }
+            throw new Exception("bug!");
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is OrType<T1, T2, T3, T4, T5, T6, T7> other)
+            {
+                return other.Representative().NullSafeEqual(Representative());
+            }
+            return false;
+        }
+    }
+
     public class OrType_1<T1, T2> : OrType<T1, T2>, IIsDefinately<T1>
     {
         public OrType_1(T1 value)
@@ -1017,6 +1194,92 @@ namespace Prototypist.Toolbox
         public override object Representative() => Value;
     }
 
+
+    public class OrType_1<T1, T2, T3, T4, T5, T6, T7, T8> : OrType<T1, T2, T3, T4, T5, T6, T7, T8>, IIsDefinately<T1>
+    {
+        public OrType_1(T1 value)
+        {
+            this.Value = value;
+        }
+
+        public T1 Value { get; }
+        public override object Representative() => Value;
+    }
+    public class OrType_2<T1, T2, T3, T4, T5, T6, T7, T8> : OrType<T1, T2, T3, T4, T5, T6, T7, T8>, IIsDefinately<T2>
+    {
+        public OrType_2(T2 value)
+        {
+            this.Value = value;
+        }
+
+        public T2 Value { get; }
+        public override object Representative() => Value;
+    }
+    public class OrType_3<T1, T2, T3, T4, T5, T6, T7, T8> : OrType<T1, T2, T3, T4, T5, T6, T7, T8>, IIsDefinately<T3>
+    {
+        public OrType_3(T3 value)
+        {
+            this.Value = value;
+        }
+
+        public T3 Value { get; }
+        public override object Representative() => Value;
+    }
+    public class OrType_4<T1, T2, T3, T4, T5, T6, T7, T8> : OrType<T1, T2, T3, T4, T5, T6, T7, T8>, IIsDefinately<T4>
+    {
+        public OrType_4(T4 value)
+        {
+            this.Value = value;
+        }
+
+        public T4 Value { get; }
+        public override object Representative() => Value;
+    }
+    public class OrType_5<T1, T2, T3, T4, T5, T6, T7, T8> : OrType<T1, T2, T3, T4, T5, T6, T7, T8>, IIsDefinately<T5>
+    {
+        public OrType_5(T5 value)
+        {
+            this.Value = value;
+        }
+
+        public T5 Value { get; }
+        public override object Representative() => Value;
+    }
+
+    public class OrType_6<T1, T2, T3, T4, T5, T6, T7, T8> : OrType<T1, T2, T3, T4, T5, T6, T7, T8>, IIsDefinately<T6>
+    {
+        public OrType_6(T6 value)
+        {
+            this.Value = value;
+        }
+
+        public T6 Value { get; }
+        public override object Representative() => Value;
+    }
+
+
+    public class OrType_7<T1, T2, T3, T4, T5, T6, T7, T8> : OrType<T1, T2, T3, T4, T5, T6, T7, T8>, IIsDefinately<T7>
+    {
+        public OrType_7(T7 value)
+        {
+            this.Value = value;
+        }
+
+        public T7 Value { get; }
+        public override object Representative() => Value;
+    }
+
+    public class OrType_8<T1, T2, T3, T4, T5, T6, T7, T8> : OrType<T1, T2, T3, T4, T5, T6, T7, T8>, IIsDefinately<T8>
+    {
+        public OrType_8(T8 value)
+        {
+            this.Value = value;
+        }
+
+        public T8 Value { get; }
+        public override object Representative() => Value;
+    }
+
     public class OrType {
 
         public static OrType<T1, T2> Make<T1, T2>(T1 value) => new OrType_1<T1,T2>(value);
@@ -1052,6 +1315,15 @@ namespace Prototypist.Toolbox
         public static OrType<T1, T2, T3, T4, T5, T6, T7> Make<T1, T2, T3, T4, T5, T6, T7>(T5 value) => new OrType_5<T1, T2, T3, T4, T5, T6, T7>(value);
         public static OrType<T1, T2, T3, T4, T5, T6, T7> Make<T1, T2, T3, T4, T5, T6, T7>(T6 value) => new OrType_6<T1, T2, T3, T4, T5, T6, T7>(value);
         public static OrType<T1, T2, T3, T4, T5, T6, T7> Make<T1, T2, T3, T4, T5, T6, T7>(T7 value) => new OrType_7<T1, T2, T3, T4, T5, T6, T7>(value);
+
+        public static OrType<T1, T2, T3, T4, T5, T6, T7, T8> Make<T1, T2, T3, T4, T5, T6, T7, T8>(T1 value) => new OrType_1<T1, T2, T3, T4, T5, T6, T7, T8>(value);
+        public static OrType<T1, T2, T3, T4, T5, T6, T7, T8> Make<T1, T2, T3, T4, T5, T6, T7, T8>(T2 value) => new OrType_2<T1, T2, T3, T4, T5, T6, T7, T8>(value);
+        public static OrType<T1, T2, T3, T4, T5, T6, T7, T8> Make<T1, T2, T3, T4, T5, T6, T7, T8>(T3 value) => new OrType_3<T1, T2, T3, T4, T5, T6, T7, T8>(value);
+        public static OrType<T1, T2, T3, T4, T5, T6, T7, T8> Make<T1, T2, T3, T4, T5, T6, T7, T8>(T4 value) => new OrType_4<T1, T2, T3, T4, T5, T6, T7, T8>(value);
+        public static OrType<T1, T2, T3, T4, T5, T6, T7, T8> Make<T1, T2, T3, T4, T5, T6, T7, T8>(T5 value) => new OrType_5<T1, T2, T3, T4, T5, T6, T7, T8>(value);
+        public static OrType<T1, T2, T3, T4, T5, T6, T7, T8> Make<T1, T2, T3, T4, T5, T6, T7, T8>(T6 value) => new OrType_6<T1, T2, T3, T4, T5, T6, T7, T8>(value);
+        public static OrType<T1, T2, T3, T4, T5, T6, T7, T8> Make<T1, T2, T3, T4, T5, T6, T7, T8>(T7 value) => new OrType_7<T1, T2, T3, T4, T5, T6, T7, T8>(value);
+        public static OrType<T1, T2, T3, T4, T5, T6, T7, T8> Make<T1, T2, T3, T4, T5, T6, T7, T8>(T8 value) => new OrType_8<T1, T2, T3, T4, T5, T6, T7, T8>(value);
 
     }
 
